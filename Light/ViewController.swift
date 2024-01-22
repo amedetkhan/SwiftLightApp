d37ed89595226a9c8621d284a90ad965da432146
Adamali Medetkhan <83174356+amedetkhan@users.noreply.github.com>
2024-01-21 20:06:42 -0800

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Medetkhan Adamali on 1/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
